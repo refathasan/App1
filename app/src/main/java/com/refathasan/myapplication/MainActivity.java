455ef1bf4b3949a16bbd5bfa1ba4e324fafbb4e8
refat hasan <refat.hasan.aiub@gmail.com>
2019-11-20 18:04:19 +0600

    version 1.0.0
    + Add Open Cv Dependencies
    + Test The OpenCv DepenDencies

--- app/src/main/java/com/refathasan/myapplication/MainActivity.java
+++ app/src/main/java/com/refathasan/myapplication/MainActivity.java
@@ -3,12 +3,25 @@ package com.refathasan.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import org.opencv.android.OpenCVLoader;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static String TAG ="MainActivity";
+    static {
+        if(OpenCVLoader.initDebug()){
+            Log.d(TAG,"Open CV connected successfully");
+        }else {
+            Log.d(TAG,"Failed!!");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
